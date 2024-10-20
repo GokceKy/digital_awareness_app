11712a7a8ab69b0e3c34594adc941dbdb484d0c5
gokceKy <gaia.gkce@gmail.com>
2024-10-20 18:38:58 +0300

    Create login_screen

---
+++ lib/view/auth/login_screen.dart
@@ -0,0 +1,98 @@
+import 'package:digital_awareness_app/product/widgets/auth_button.dart';
+import 'package:digital_awareness_app/product/widgets/textfield.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                //Logo
+                Image.asset(
+                  'assets/ikontwo.png', // PNG dosyasının yolu
+                  width: 100, // Genişlik
+                  height: 100, // Yükseklik
+                ),
+                const SizedBox(height: 50),
+                Text(
+                  "Welcome back you\'ve been missed!",
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 50),
+
+                Textfield(
+                  controller: usernameController,
+                  hintText: 'username',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 10),
+
+                Textfield(
+                  controller: passwordController,
+                  hintText: 'password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 25),
+                AuthButton(
+                  onTap: signUserIn,
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Not a member?",
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    const SizedBox(width: 4),
+                    Text(
+                      "Register now",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+
+                //not a member
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
