d67799298764fe73bd5655661bd2d52dc390b5ed
gokceKy <gaia.gkce@gmail.com>
2024-10-20 23:59:33 +0300

    create auth_screen

---
+++ lib/view/auth/auth_screen.dart
@@ -0,0 +1,27 @@
+import 'package:digital_awareness_app/main.dart';
+import 'package:digital_awareness_app/view/auth/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            // user is logged in
+            if (snapshot.hasData) {
+              return HomeScreen();
+            }
+            //user is Not Logged in
+            else {
+              return LoginScreen();
+            }
+          }),
+    );
+  }
+}
